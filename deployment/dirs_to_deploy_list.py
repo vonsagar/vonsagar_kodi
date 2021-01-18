0fb3e750de4f8d99ddc8d51981f359ffb4c9a38f
vonsagar <przkawecki@gmail.com>
2021-01-18 07:43:25 +0100

    Add openplayers repo (openmeta)

--- deployment/dirs_to_deploy_list.py
+++ deployment/dirs_to_deploy_list.py
@@ -3,5 +3,6 @@ import os
 __DIRS_TO_DEPLOY_LIST__ = [{"id": "seren",       "value": {"name": os.path.join("..", "plugin.video.seren"),         "dest": os.path.join(".kodi", "addons")}},
                            {"id": "fullmatchtv", "value": {"name": os.path.join("..", "plugin.video.fullmatchtv"),   "dest": os.path.join(".kodi", "addons")}},
                            {"id": "autosubs",    "value": {"name": os.path.join("..", "service.autosubs"),           "dest": os.path.join(".kodi", "addons")}},
-                           {"id": "subloader",   "value": {"name": os.path.join("..", "service.subloader"),          "dest": os.path.join(".kodi", "addons")}}
+                           {"id": "subloader",   "value": {"name": os.path.join("..", "service.subloader"),          "dest": os.path.join(".kodi", "addons")}},
+                           {"id": "openplayers", "value": {"name": os.path.join("..", "Players"),                    "dest": os.path.join(".kodi", "userdata", "addon_data", "plugin.video.openmeta", "Players")}}
                            ]
