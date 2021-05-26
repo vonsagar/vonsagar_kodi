408171d8b1b43e311b6f7b8708a0d19092c929ce
vonsagar <przkawecki@gmail.com>
2021-05-26 17:51:30 +0200

    Fix paths to zips

--- deployment/remote_hosts_list.py
+++ deployment/remote_hosts_list.py
@@ -1,3 +1,3 @@
-__REMOTE_HOSTS_LIST__ = [{"id": "Jack",   "address": "192.168.1.108", "username": "root", "password": "coreelec"},
+__REMOTE_HOSTS_LIST__ = [#{"id": "Jack",   "address": "192.168.1.108", "username": "root", "password": "coreelec"},
                          {"id": "Johnny", "address": "192.168.1.107", "username": "root", "password": "coreelec"}
                          ]
