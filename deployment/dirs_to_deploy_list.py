7a3e2ab40dc7cf2b1dc14fc6c63e180786d69225
vonsagar <przkawecki@gmail.com>
2021-05-26 17:32:01 +0200

    Add fanfilm and repository to deployment

--- deployment/dirs_to_deploy_list.py
+++ deployment/dirs_to_deploy_list.py
@@ -4,5 +4,7 @@ __DIRS_TO_DEPLOY_LIST__ = [{"id": "seren",       "value": {"name": "plugin.video
                            {"id": "fullmatchtv", "value": {"name": "plugin.video.fullmatchtv",   "dest": os.path.join(".kodi", "addons")}},
                            {"id": "autosubs",    "value": {"name": "service.autosubs",           "dest": os.path.join(".kodi", "addons")}},
                            {"id": "subloader",   "value": {"name": "service.subloader",          "dest": os.path.join(".kodi", "addons")}},
+                           {"id": "fanfilm",     "value": {"name": "plugin.video.fanfilm",       "dest": os.path.join(".kodi", "addons")}},
+                           {"id": "repository",  "value": {"name": "repository.vonsagar",        "dest": os.path.join(".kodi", "addons")}},
                            {"id": "openplayers", "value": {"name": "Players",                    "dest": os.path.join(".kodi", "userdata", "addon_data", "plugin.video.openmeta", "Players")}}
                            ]
